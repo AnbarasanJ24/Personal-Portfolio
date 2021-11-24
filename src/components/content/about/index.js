b98b8aa11fcc9b9452ddd880e696670490435544
Anbarasan <anbarasanj24@gmail.com>
2021-11-24 17:12:51 +0530

    Added About me section

---
+++ src/components/content/about/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './about.css';
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="about-me">
+                <h1>Hello There, I am Anbarasan
+                </h1>
+                <h4>
+                    Proficient front-end engineer with 2.5 years of experience.
+                    Enhanced web performance & security using the relevant tech stack.
+                </h4>
+            </div>
+            <div className="profile_image">
+                Image
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import About from "./about";
+import Contact from "./contact";
 import './content.css';
+import Projects from "./projects";
+import Skills from "./skills";
+import Work from "./work";
 
 const Content = () => {
     return (
-        <section>
-            <h1>Content</h1>
-        </section>
+        <main className="main">
+            <section id="about">
+                <About />
+            </section>
+            <section id="project">
+                <Projects />
+            </section>
+            <section id="skills">
+                <Skills />
+            </section>
+            <section id="work">
+                <Work />
+            </section>
+            <section id="contact">
+                <Contact />
+            </section>
+        </main>
     )
 }
 
